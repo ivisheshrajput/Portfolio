ec83607804884a3244355944c46d941faf25bd19
Vishesh Rajput <156611608+ivisheshrajput@users.noreply.github.com>
2024-03-24 01:09:49 +0530

    Add files via upload

--- components/ResumeBtn.js
+++ components/ResumeBtn.js
@@ -15,22 +15,21 @@ const ResumeBtn = () => {
     downloadLink.click();
   };
   return (
-  
     <div className="mx-auto xl:mx-0">
-        <Link href="/VisheshRajput.pdf" target="_blank">
-      <div className="cursor-pointer  relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group ">
-        <Image
-          src={"/rounded-text.png"}
-          width={141}
-          height={148}
-          alt=""
-          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px] cursor-pointer "
-        />
-        <HiArrowRight
-          onClick={handleDownload}
-          className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300 cursor-pointer  "
-        />
-      </div>
+      <Link href="/VisheshRajput.pdf" target="_blank">
+        <div className="cursor-pointer  relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group ">
+          <Image
+            src={"/rounded-text.png"}
+            width={141}
+            height={148}
+            alt=""
+            className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px] cursor-pointer "
+          />
+          <HiArrowRight
+            onClick={handleDownload}
+            className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300 cursor-pointer  "
+          />
+        </div>
       </Link>
     </div>
   );
--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -11,6 +11,7 @@ import { Navigation, Pagination } from "swiper";
 
 //icons
 import { BsArrowRight } from "react-icons/bs";
+import Link from "next/link";
 
 // data
 const workSlides = {
@@ -18,40 +19,48 @@ const workSlides = {
     {
       images: [
         {
-          title: "title",
-          path: "/thumb1.jpg",
+          title: "Food Panda",
+          path: "/Thumb1.png",
+          link: "https://food-panda-ten.vercel.app/",
         },
         {
-          title: "title",
-          path: "/thumb2.jpg",
+          title: "Food Panda",
+          path: "/Thumb2.png",
+          link: "https://food-panda-ten.vercel.app/",
         },
         {
-          title: "title",
-          path: "/thumb3.jpg",
+          title: "Food Panda",
+          path: "/Thumb3.png",
+          link: "https://food-panda-ten.vercel.app/",
         },
         {
-          title: "title",
-          path: "/thumb4.jpg",
+          title: "Food Panda",
+          path: "/Thumb4.png",
+          link: "https://food-panda-ten.vercel.app/",
         },
       ],
     },
     {
       images: [
         {
-          title: "title",
-          path: "/thumb4.jpg",
+          title: "Cinebility",
+          path: "/Thumb5.png",
+          link: "https://cinebility.web.app/",
         },
         {
-          title: "title",
-          path: "/thumb1.jpg",
+          title: "Cinebility",
+          path: "/Thumb6.png",
+          link: "https://cinebility.web.app/",
         },
         {
-          title: "title",
-          path: "/thumb2.jpg",
+          title: "Cinebility",
+          path: "/Thumb7.png",
+          link: "https://cinebility.web.app/",
         },
         {
-          title: "title",
-          path: "/thumb3.jpg",
+          title: "Cinebility",
+          path: "/Thumb8.png",
+          link: "https://cinebility.web.app/",
         },
       ],
     },
@@ -77,32 +86,35 @@ const WorkSlider = () => {
                     key={index}
                     className="relative rounded-lg overflow-hidden flex items-center justify-center group "
                   >
-                    <div className="flex items-center justify-center relative overflow-hidden group">
-                      {/* Image */}
-                      <Image
-                        src={image.path}
-                        width={500}
-                        height={300}
-                        alt={""}
-                      />
-                      {/* overlay gradient */}
-                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#8e70e8] to-[#f494e4] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
-                      {/* title */}
-                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                        <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
-                          {/* title part1 */}
-                          <div className="delay-100  ">LIVE</div>
-                          {/* title part2 */}
-                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150 ">
-                            PROJECT
-                          </div>
-                          {/* icon */}
-                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200 ">
-                            <BsArrowRight />
+                    <Link href={image.link}>
+                      <div className="flex items-center justify-center relative overflow-hidden group">
+                        {/* Image */}
+                        <Image
+                          src={image.path}
+                          width={500}
+                          height={300}
+                          alt={""}
+                        />
+
+                        {/* overlay gradient */}
+                        <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#8e70e8] to-[#f494e4] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                        {/* title */}
+                        <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                          <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                            {/* title part1 */}
+                            <div className="delay-100  ">LIVE</div>
+                            {/* title part2 */}
+                            <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150 ">
+                              PROJECT
+                            </div>
+                            {/* icon */}
+                            <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200 ">
+                              <BsArrowRight />
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
+                    </Link>
                   </div>
                 );
               })}
